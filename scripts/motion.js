f05f82010294dffd343bc7a9000227287e74005b
Thomas Balthasar <thomasblthsr@gmail.com>
2024-01-07 15:13:17 +0100

    test

--- scripts/motion.js
+++ scripts/motion.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function handleMotion(event) {
-        //document.getElementById('output').textContent = event.acceleration.x;
+        document.getElementById('output').textContent = event.acceleration.x;
         if(!motionHandler.finished){
             if(Math.abs(event.acceleration.x) > 2){
                 document.getElementById('output').textContent = 'in motion';
