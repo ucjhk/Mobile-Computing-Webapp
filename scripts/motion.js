6338c9f2eca405252d45df0487c27c951f8fbeb6
Thomas Balthasar <thomasblthsr@gmail.com>
2024-01-07 13:05:22 +0100

    test

--- scripts/motion.js
+++ scripts/motion.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
           DeviceMotionEvent.requestPermission()
             .then(permissionState => {
               if (permissionState === 'granted') {
-                window.addEventListener('devicemotion', handleMotion, true);
+                window.addEventListener('deviceorientation', handleMotion, true);
               }
             })
             .catch(console.error);
