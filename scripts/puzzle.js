ea29735cdf3318267e22e4dffd2e251ddf72224c
Thomas Balthasar <thomasblthsr@gmail.com>
2024-01-07 16:20:51 +0100

    test

--- scripts/puzzle.js
+++ scripts/puzzle.js
@@ -10,22 +10,171 @@ function shuffelPieces(){
     });
 }
 
-
 function toogleButton() {
-    hideButton();
     shuffelPieces();
+    startPuzzle();
+}
+
+function startPuzzle(){
+    hideStart();
     timer.start();
     window.scrollTo(0, document.body.scrollHeight);
 }
 
-function hideButton() {
-    var button = document.getElementById('start-btn');
-    button.style.visibility = 'hidden';
+function getStart() {
+    if(document.getElementById('start-btn').style.display == 'none'){
+        return document.getElementById('start-txt');
+    }
+    else{
+        return document.getElementById('start-btn');
+    }
 }
 
-function showButton() {
-    var button = document.getElementById('start-btn');
-    button.style.visibility = 'visible';
+function hideStart() {
+    getStart().style.visibility = 'hidden';
+}
+
+function showStart() {
+    getStart().style.visibility = 'visible';
+}
+
+// Function to create puzzle pieces
+function createPuzzlePieces(pieceSize = 9, boardSize = 300) {
+    const puzzleContainer = document.getElementById('puzzle-container');
+    const puzzleBoard = document.getElementById('puzzle-board');
+
+    // Array to store puzzle pieces
+    const puzzlePieces = [];
+
+    // Create drop targets on the puzzle board
+    for (let i = 0; i < pieceSize; i++) {
+        const dropTarget = document.createElement('div');
+        dropTarget.classList.add('drop-target');
+        dropTarget.dataset.targetId = i.toString(); // Unique identifier for the drop target
+        puzzleBoard.appendChild(dropTarget);
+
+        // Event listener for drag over on the drop target
+        dropTarget.addEventListener('dragover', (event) => {
+            event.preventDefault();
+        });
+
+        // Event listener for drop on the drop target
+        dropTarget.addEventListener('drop', handleDrop);
+    }
+
+    for (let i = 0; i < pieceSize; i++) {
+        const piece = document.createElement('div');
+        piece.classList.add('puzzle-piece');
+        piece.dataset.pieceId = i.toString(); // Unique identifier for the puzzle piece
+        piece.style.backgroundImage = `url(${imageUrl})`;
+        piece.style.backgroundPosition = `-${(i % Math.sqrt(pieceSize)) * (boardSize / Math.sqrt(pieceSize))}px
+         -${Math.floor(i / Math.sqrt(pieceSize)) * (boardSize / Math.sqrt(pieceSize))}px`;
+
+        // Set the draggable attribute
+        piece.draggable = false;
+
+        // Event listener for drag start
+        piece.addEventListener('dragstart', (event) => {
+            event.dataTransfer.setData('text/plain', i.toString());
+        });
+
+        puzzlePieces.push(piece);
+    }
+
+    puzzlePieces.forEach(piece => {
+        puzzleContainer.appendChild(piece);
+    });
+}
+
+function resetContainers(pieceSize){
+    const puzzleContainer = document.getElementById('puzzle-container');
+    const puzzleBoard = document.getElementById('puzzle-board');
+    while (puzzleContainer.firstChild) {
+        puzzleContainer.removeChild(puzzleContainer.firstChild);
+    }
+    while(puzzleBoard.firstChild){
+        puzzleBoard.removeChild(puzzleBoard.firstChild);
+    }
+    createPuzzlePieces(pieceSize);
+    showStart();
+}
+
+function changeSize(value){
+    document.documentElement.style.setProperty('--js-piece-count', value);
+    resetContainers(value);
+}
+
+function changeImage(value){
+    if (value.files && value.files[0]) {
+        imageUrl = URL.createObjectURL(value.files[0]);
+        resetContainers(document.getElementById('select-number').value);
+    }
+}
+
+// Function to handle drop event on puzzle pieces
+function handleDrop(event) {
+    event.preventDefault();
+
+    const pieceIndex = event.dataTransfer.getData('text/plain');
+    const draggedPiece = document.querySelector(`.puzzle-piece[data-piece-id="${pieceIndex}"]`);
+    const puzzleContainer = document.getElementById('puzzle-container');
+
+    if(!draggedPiece.draggable) return;
+
+    // Check if the drop area is occupied
+    if (event.target.classList.contains('drop-target')) {
+        // Append the dragged piece to the drop target
+        event.target.appendChild(draggedPiece);
+    } else {
+        // DraggedPiece already in drop target
+        parent = event.target.parentElement;
+        if(draggedPiece.parentElement.classList.contains('drop-target') && parent.classList.contains('drop-target')){
+            draggedPieceParent = draggedPiece.parentElement;
+            parent.appendChild(draggedPiece);
+            draggedPieceParent.appendChild(event.target);
+        }
+        // Drop area is occupied, return the piece to the selection box
+        else{
+            puzzleContainer.appendChild(event.target);
+            parent.appendChild(draggedPiece);
+        }
+    }
+
+    // Check if the puzzle is solved
+    checkPuzzleSolved();
+}
+
+// Function to check if the puzzle is solved
+function checkPuzzleSolved() {
+    const puzzleBoard = document.getElementById('puzzle-board');
+    const dropTargets = puzzleBoard.querySelectorAll('.drop-target');
+
+    // Check if all drop targets have the correct puzzle piece
+    const isSolved = Array.from(dropTargets).every(target => {
+        const piece = target.firstElementChild;
+        if (piece) {
+            return piece.dataset.pieceId === target.dataset.targetId;
+        }
+        return false;
+    });
+
+    if (isSolved) {
+        puzzleSolved();
+    }
+}
+
+function puzzleSolved(){
+    showStart();
+    time = timer.stop();
+    console.log(time);
+    resetContainers(document.getElementById('select-number').value);
+    // Puzzle solved, display the success screen
+    solvedScreen = document.getElementById('solved-screen');
+    solvedScreen.querySelector('.time').textContent = "in "+ time.toString() + " seconds";
+    solvedScreen.classList.remove('show');
+    void solvedScreen.offsetWidth;
+    solvedScreen.classList.add('show');
+    //solvedScreen.style.display = 'block';
 }
 
 
@@ -54,143 +203,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Create puzzle pieces and drop targets on page load
     createPuzzlePieces();
 
-    // Function to create puzzle pieces
-    function createPuzzlePieces(pieceSize = 9, boardSize = 300) {
-        const puzzleContainer = document.getElementById('puzzle-container');
-        const puzzleBoard = document.getElementById('puzzle-board');
-
-        // Array to store puzzle pieces
-        const puzzlePieces = [];
-
-        // Create drop targets on the puzzle board
-        for (let i = 0; i < pieceSize; i++) {
-            const dropTarget = document.createElement('div');
-            dropTarget.classList.add('drop-target');
-            dropTarget.dataset.targetId = i.toString(); // Unique identifier for the drop target
-            puzzleBoard.appendChild(dropTarget);
-
-            // Event listener for drag over on the drop target
-            dropTarget.addEventListener('dragover', (event) => {
-                event.preventDefault();
-            });
-
-            // Event listener for drop on the drop target
-            dropTarget.addEventListener('drop', handleDrop);
-        }
-
-        for (let i = 0; i < pieceSize; i++) {
-            const piece = document.createElement('div');
-            piece.classList.add('puzzle-piece');
-            piece.dataset.pieceId = i.toString(); // Unique identifier for the puzzle piece
-            piece.style.backgroundImage = `url(${imageUrl})`;
-            piece.style.backgroundPosition = `-${(i % Math.sqrt(pieceSize)) * (boardSize / Math.sqrt(pieceSize))}px
-             -${Math.floor(i / Math.sqrt(pieceSize)) * (boardSize / Math.sqrt(pieceSize))}px`;
-
-            // Set the draggable attribute
-            piece.draggable = false;
-
-            // Event listener for drag start
-            piece.addEventListener('dragstart', (event) => {
-                event.dataTransfer.setData('text/plain', i.toString());
-            });
-
-            puzzlePieces.push(piece);
-        }
-
-        puzzlePieces.forEach(piece => {
-            puzzleContainer.appendChild(piece);
-        });
-    }
-
-    function resetContainers(pieceSize){
-        const puzzleContainer = document.getElementById('puzzle-container');
-        const puzzleBoard = document.getElementById('puzzle-board');
-        while (puzzleContainer.firstChild) {
-            puzzleContainer.removeChild(puzzleContainer.firstChild);
-        }
-        while(puzzleBoard.firstChild){
-            puzzleBoard.removeChild(puzzleBoard.firstChild);
-        }
-        createPuzzlePieces(pieceSize);
-        showButton();
-    }
-
-    function changeSize(value){
-        document.documentElement.style.setProperty('--js-piece-count', value);
-        resetContainers(value);
-    }
-
-    function changeImage(value){
-        if (value.files && value.files[0]) {
-            imageUrl = URL.createObjectURL(value.files[0]);
-            resetContainers(document.getElementById('select-number').value);
-        }
-    }
-
-    // Function to handle drop event on puzzle pieces
-    function handleDrop(event) {
-        event.preventDefault();
-
-        const pieceIndex = event.dataTransfer.getData('text/plain');
-        const draggedPiece = document.querySelector(`.puzzle-piece[data-piece-id="${pieceIndex}"]`);
-        const puzzleContainer = document.getElementById('puzzle-container');
-
-        if(!draggedPiece.draggable) return;
-
-        // Check if the drop area is occupied
-        if (event.target.classList.contains('drop-target')) {
-            // Append the dragged piece to the drop target
-            event.target.appendChild(draggedPiece);
-        } else {
-            // DraggedPiece already in drop target
-            parent = event.target.parentElement;
-            if(draggedPiece.parentElement.classList.contains('drop-target') && parent.classList.contains('drop-target')){
-                draggedPieceParent = draggedPiece.parentElement;
-                parent.appendChild(draggedPiece);
-                draggedPieceParent.appendChild(event.target);
-            }
-            // Drop area is occupied, return the piece to the selection box
-            else{
-                puzzleContainer.appendChild(event.target);
-                parent.appendChild(draggedPiece);
-            }
-        }
-
-        // Check if the puzzle is solved
-        checkPuzzleSolved();
-    }
-
-    // Function to check if the puzzle is solved
-    function checkPuzzleSolved() {
-        const puzzleBoard = document.getElementById('puzzle-board');
-        const dropTargets = puzzleBoard.querySelectorAll('.drop-target');
-
-        // Check if all drop targets have the correct puzzle piece
-        const isSolved = Array.from(dropTargets).every(target => {
-            const piece = target.firstElementChild;
-            if (piece) {
-                return piece.dataset.pieceId === target.dataset.targetId;
-            }
-            return false;
-        });
-
-        if (isSolved) {
-            puzzleSolved();
-        }
-    }
-
-    function puzzleSolved(){
-        showButton();
-        time = timer.stop();
-        console.log(time);
-        resetContainers(document.getElementById('select-number').value);
-        // Puzzle solved, display the success screen
-        solvedScreen = document.getElementById('solved-screen');
-        solvedScreen.querySelector('.time').textContent = "in "+ time.toString() + " seconds";
-        solvedScreen.classList.remove('show');
-        void solvedScreen.offsetWidth;
-        solvedScreen.classList.add('show');
-        //solvedScreen.style.display = 'block';
-    }
+    
 });
 
